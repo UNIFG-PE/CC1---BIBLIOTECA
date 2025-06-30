21f9c60448ab4af8576f521b0d8a9ed74b018ff1
Luiz Domingos <luiz.silva@truechange.com>
2025-06-30 09:29:03 -0300

    Inclusão da parte Model Livro

--- src/com/biblioteca/app/model/Livro.java
+++ src/com/biblioteca/app/model/Livro.java
@@ -1,5 +1,94 @@
 package com.biblioteca.app.model;
 
-public class Livro {
+import java.io.Serializable;
+import java.util.Objects;
 
+public class Livro implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private int anoPublicacao;
+    private boolean disponivel;
+
+
+    public Livro (String titulo, String autor, String isbn, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.anoPublicacao = anoPublicacao;
+        this.disponivel = true;
+
+    }
+
+    public String getTitulo() {
+        return titulo;
+
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return Objects.equals(isbn, livro.isbn);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(isbn);
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Livro{"
+                        +"titulo='"
+                        + titulo
+                        + '\''
+                        +
+                        ", autor='"
+                        + autor
+                        + '\''
+                        +
+                        ", isbn='"
+                        + isbn
+                        + '\''
+                        +
+                        ", anoPublicacao="
+                        + anoPublicacao
+                        +
+                        ", disponivel="
+                        + (disponivel ? "Sim" : "Não")
+                        +
+                        '}';
+    }
 }
