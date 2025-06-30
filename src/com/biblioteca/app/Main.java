f12ad2b661c1f5b7d724148d21cfad3a64fdceed
Nunes-01 <carlos.contato34@gmail.com>
2025-06-29 23:51:19 -0300

    Classe Main

--- src/com/biblioteca/app/Main.java
+++ src/com/biblioteca/app/Main.java
@@ -1,10 +1,12 @@
 package com.biblioteca.app;
 
+import com.biblioteca.app.view.MenuView;
+
 public class Main {
 
 	public static void main(String[] args) {
-	
 
+		MenuView.exibirMenuPrincipal();
 	}
 
 }
